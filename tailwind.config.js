8494140c2b6697cbaccfd9a61dd9f9729c08c779
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-19 17:47:38 -0500

    fixed more crap

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,13 @@
-content: [
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}"
-],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './cms/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
