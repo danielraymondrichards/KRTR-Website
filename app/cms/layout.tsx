47775b9dd253c2f9b0657aee1d3e9c46233256fc
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-20 08:15:15 -0500

    Update layout.tsx

--- app/cms/layout.tsx
+++ app/cms/layout.tsx
@@ -3,7 +3,7 @@ import { ClerkProvider, useUser } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 import { useEffect } from 'react';
 import { syncUser } from '@/lib/syncUserToSupabase';
-import '../../cms.css';
+import '../styles/cms.css';
 
 
 
