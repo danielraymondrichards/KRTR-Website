db85b191063081ab3ac353004cf1fcb76eddc162
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-22 11:55:15 -0500

    Update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,8 +1,8 @@
 import { getAuth } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { createClient } from '@/utils/supabase/server';
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   const { userId } = getAuth(req);
   if (!userId) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
 
