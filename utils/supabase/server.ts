72d85468dec1052aceda1199a00a61df90d19678
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-21 22:00:19 -0500

    updated gitignore, created server.ts

---
+++ utils/supabase/server.ts
@@ -0,0 +1,12 @@
+import { createServerClient } from '@supabase/ssr';
+import { cookies } from 'next/headers';
+import { Database } from '@/types/supabase';
+
+export const createClient = () =>
+  createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, // or SERVICE_ROLE_KEY if you're doing upserts
+    {
+      cookies,
+    }
+  );
