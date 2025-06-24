7ba084292cc71301670fa08608d23ccd380cd408
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-24 16:49:10 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,3 @@
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
 
 export const supabase = createPagesBrowserClient();
-  process.env.NEXT_PUBLIC_SUPABASE_URL!
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
