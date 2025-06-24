bb88ac291fc636c6bdf0782269891a3962761b2b
Daniel Raymond Richards <danielraymondrichards@gmail.com>
2025-06-24 14:29:01 -0500

    Update createUpload.ts

--- app/api/mux/createUpload.ts
+++ app/api/mux/createUpload.ts
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
 
   const upload = await mux.video.uploads.create({
-    new_asset_settings: { playback_policy: 'public' },
-    playback_policy: 'public',
+    new_asset_settings: { playback_policy: '[public]' },
+    playback_policy: '[public]',
   });
 
   res.status(200).json({
